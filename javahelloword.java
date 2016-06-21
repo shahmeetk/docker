7d347d7e275e5fe7271d06bf119c15f7919d246b
Meet Shah <meet.shah@sophos.com>
2016-06-21 15:52:14 +0530

    first commit

---
+++ javahelloword.java
@@ -0,0 +1,8 @@
+public class javahelloworld
+{
+	public static void main (String [] args)
+	{
+		System.out.println("You have enter in world of java");
+		System.out.println("This is Meet  Shah");
+	}
+}
